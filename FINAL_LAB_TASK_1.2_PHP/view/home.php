255cdd27a5500f96df5ff1f18abfb7bcd8b2a493
ZISHA <you@example.com>
2020-12-01 09:26:16 +0600

    commit last

--- FINAL_LAB_TASK_1.2_PHP/view/home.php
+++ FINAL_LAB_TASK_1.2_PHP/view/home.php
@@ -1,50 +1,54 @@
+
 <?php
+	include('header.php');
+	require_once('../model/userService.php');
+	$userlist = getAllUser();
 
-	if(isset($_GET['msg'])){
-		
-		if($_GET['msg'] == "invalid_user"){
-			echo "username/password not valid...";
-		}
-
-		if($_GET['msg'] == "null_username"){
-			echo "Username field is empty...";
-		}
-
-		if($_GET['msg'] == "null_password"){
-			echo "Password field is empty...";
-		}
-	}
-
-	/*if(isset($_COOKIE['rm'])){
-		header('location: home.php');
-	}*/
 ?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login Page</title>
+	<title> Home Page</title>
 </head>
 <body>
-	<form method="post" action="../php/loginCheck.php">
-		<fieldset>
-			<legend>LOGIN</legend>
-			<table>
-				<tr>
-					<td>Username</td>
-					<td><input type="text" name="username" ></td>
-				</tr>
-				<tr>
-					<td>Password</td>
-					<td><input type="password" name="password"></td>
-				</tr>
-				
-				<tr>
-					<td></td>
-					<td><input type="submit" name="submit" value="Submit"></td>
-				</tr>
-			</table>
-		</fieldset>
-	</form>
+	<h1>Welcome home, </h1>
+	<a href="create.php">Create New User</a> |
+	<a href="profile.php">Profile</a> |
+	<a href="userlist.php">User List</a> |
+	<a href="../php/logout.php">logout</a>
+
+	<h3>User list</h3>
+	<table border="1"> 
+		<tr>
+			<td>ID</td>
+			<td>USERNAME</td>
+			<td>PASSWORD</td>
+			<td>EMAIL</td>
+			<td>TYPE</td>
+			<td>Edit</td>
+			<td>Delete</td>
+		</tr>
+
+		<?php for($i=0; $i< count($userlist); $i++){ 
+			$un = $userlist[$i]['username'];
+			?>
+
+			<tr>
+				<td><?=$userlist[$i]['e_name']?></td>
+				<td><?=$userlist[$i]['com_name']?></td>
+				<td><?=$userlist[$i]['con_no']?></td>
+				<td><?=$userlist[$i]['username']?></td>
+				<td><?=$userlist[$i]['password']?></td>
+
+				<td><a href="edit.php?msg=<?php echo urlencode($un)?>">Edit</a></td>
+				<td><a href="../php/delete.php?msg=<?php echo urlencode($un)?>">Delete</a></td>
+			</tr>
+	<?php } ?>
+
+	</table>
+
+
 </body>
 </html>
